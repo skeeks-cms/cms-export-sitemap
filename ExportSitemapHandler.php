61cc58ae83eb7b9a3c6d66cb29f339e218e1aeb6
Semenov Alexander <semenov@skeeks.com>
2017-04-15 14:21:09 +0300

    Fixed timezone

--- ExportSitemapHandler.php
+++ ExportSitemapHandler.php
@@ -401,8 +401,9 @@ class ExportSitemapHandler extends ExportHandler
      */
     private function _lastMod($model)
     {
-        $string = "2013-08-03T21:14:41+01:00";
-        $string = date("Y-m-d", $model->updated_at) . "T" . date("H:i:s+04:00", $model->updated_at);
+        //$string = "2013-08-03T21:14:41+01:00";
+        //$string = date("Y-m-d", $model->updated_at) . "T" . date("H:i:s+04:00", $model->updated_at);
+        $string = date("с", $model->updated_at);
 
         return $string;
     }
