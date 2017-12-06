560d51aa16a112a14dccf96997b08b47b9d3a7f8
Nelya-home <nelya@rusoft.ru>
2017-12-06 12:52:15 +0300

    Add activity pages and elements. Add type page

--- src/messages/ru/main.php
+++ src/messages/ru/main.php
@@ -1,4 +1,7 @@
 <?php
 return [
     'Sitemap.xml export' => 'Генерация sitemap.xml',
+    'Active flag to contents element' => 'Учитывать флаг активности для элементов контента',
+    'Active flag to tree' => 'Учитывать флаг активности для разделов',
+    'Types of tree' => 'Укажите типы разделов',
 ];
